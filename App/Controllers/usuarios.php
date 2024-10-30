338ac84173cb3fc19be5be9706730f424939074d
Khazin217 <kauegabrielascui@gmail.com>
2024-10-30 10:27:32 -0400

    BlogAula Update

--- App/Controllers/usuarios.php
+++ App/Controllers/usuarios.php
@@ -1,18 +1,46 @@
 <?php
+
 class Usuarios extends Controller{
     public function cadastrar(){
-        // Vai fazer uma verificação se o cadastro está certo
-        $formulario = filter_input_array(
-            INPUT_POST, ['nome'=>FILTER_SANITIZE_SPECIAL_CHARS, //Para sanitizar entradas como o nome, evitando a execução de código malicioso
-            'email'=>FILTER_SANITIZE_SPECIAL_CHARS, //Para garantir que o email seja válido
-            'senha'=>FILTER_DEFAULT,
-            'confirma_senha'=>FILTER_DEFAULT]);
-        /*
-        echo $_POST['nome'];
-        echo $_POST['email'];
-        echo $_POST['senha'];
-        */
+       $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+       if(isset($formulario)):
+        $dados = [
+            'nome'=>trim($formulario['nome']),
+            'email'=>trim(($formulario['email'])),
+            'senha'=>trim(($formulario['senha'])),
+            'confirma_senha'=>trim(($formulario['confirma_senha'])),
+        ];
+       if(in_array("",$formulario)):
+            if(empty($formulario['nome'])):
+                $dados['nome_erro'] = 'Preencha o campo nome';
+            endif;
+            if(empty($formulario['email'])):
+                $dados['email_erro'] = 'Preencha o campo email';
+            endif;
+            if(empty($formulario['senha'])):
+                $dados['senha_erro'] = 'Preencha o campo senha';
+            endif;
+            if(empty($formulario['confirma_senha'])):
+                $dados['confirma_senha_erro'] = 'Preencha o campo confirma senha';
+            endif;
+        else:
+            if(strlen($formulario['senha']) < 6):
+                $dados['senha_erro'] = 'A senha deve ter no minimo 7 caracteres';
+            elseif($formulario['senha'] != $formulario['confirma_senha']):
+                    $dados['confirma_senha_erro'] = 'As senha são diferentes';
+                else:
+                    echo 'Pode cadastrar os dados<hr>';
+                endif;
+            endif;
+                var_dump($formulario);
+        else:
+            $dados = [
+                'nome' => '',
+                'email' => '',
+                'senha' => '',
+                'confirma_senha' => ''
+            ];
+        endif;
         $this->view('usuarios/cadastrar');
-    }//Fim do Cadastrar
-}// Fim da classe Cadastrar
-?>
+    }//fim da função cadastrar
+}//fim da classe Usuarios
--- App/Views/usuarios/cadastrar.php
+++ App/Views/usuarios/cadastrar.php
@@ -12,20 +12,32 @@
 
             <form name="cadastrar" method="POST" action="<?=URL?>/usuarios/cadastrar" class="mt-4">
                 <div class="form-group">
-                    <label for="nome">Nome: <sup class="text-danger">*</sup></label>
-                    <input type="text" name="nome" id="nome" class="form-control" require>
+                    <label for='nome'>Nome: <sup class='text-danger'>*</sup></label>
+                    <input type='text' name='nome' id='nome' class='form-control' require value='<?=$dados['nome']?>' class='form-control <?=$dados ['nome_erro'] ? 'is-invalid' : '' ?>'>
+                <div class='invalid-feedback'>
+                    <?$dados ['nome_erro']?>
+                </div>
                 </div>
                 <div class="form-group">
-                    <label for="email">Email: <sup class="text-danger">*</sup></label>
-                    <input type="email" name="email" id="email" class="form-control" require>
+                    <label for='email'>Email: <sup class='text-danger'>*</sup></label>
+                    <input type='email' name='email' id='email' class='form-control' require value='<?=$dados['email']?>' class='form-control <?=$dados ['email_erro'] ? 'is-invalid' : '' ?>'>
+                <div class='invalid-feedback'>
+                    <?$dados ['email_erro']?>
+                </div>
                 </div>
                 <div class="form-group">
-                    <label for="senha">Senha: <sup class="text-danger">*</sup></label>
-                    <input type="senha" name="senha" id="senha" class="form-control" require>
+                    <label for='senha'>Senha: <sup class='text-danger'>*</sup></label>
+                    <input type='senha' name='senha' id='senha' class='form-control' require value='<?=$dados['senha']?>' class='form-control <?=$dados ['senha_erro'] ? 'is-invalid' : '' ?>'>
+                <div class='invalid-feedback'>
+                    <?$dados ['senha_erro']?>
+                </div>
                 </div>
                 <div class="form-group">
-                    <label for="confirma_senha">Confirma Senha: <sup class="text-danger">*</sup></label>
-                    <input type="confirma_senha" name="confirma_senha" id="confirma_senha" class="form-control" require>
+                    <label for='confirma_senha'>Confirma Senha: <sup class='text-danger'>*</sup></label>
+                    <input type='confirma_senha' name='confirma_senha' id='confirma_senha' class='form-control' require value='<?=$dados['confirma_senha']?>' class='form-control <?=$dados ['confirma_senha_erro'] ? 'is-invalid' : '' ?>'>
+                <div class='invalid-feedback'>
+                    <?$dados ['senha_erro']?>
+                </div>
                 </div>
                 <div class="row">
                     <div class="col">
